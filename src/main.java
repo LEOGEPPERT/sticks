17a7c0ed5ff5ce34b3b8c65a38a4ef7ff4e65c5b
Leo G <lgeppert1145@stu.neisd.net>
2024-04-01 10:36:43 -0500

    creating cpu opponent, need to add stuff for methods

--- src/main.java
+++ src/main.java
@@ -7,14 +7,24 @@ public class main {
         int stickA2 = 1;
         int stickB1 = 1;
         int stickB2 = 1;
+        Map<Integer, String> botChoice = new TreeMap<>();
+        botChoice.put(1, "attack");
+        botChoice.put(2, "switch");
+        
         System.out.println("Sticks");
         Scanner scan = new Scanner(System.in);
         //game will run while the value is true
         while(game){
         
-        boolean turn = true; //if true, it is player 1's turn, if false, it is player 2's turn 
-        System.out.println("PLAYER 1: What do want to do?");
-        String choice = scan.nextLine();
+        boolean turn = true; //if true, it is player 1's turn, if false, it is player 2's turn
+        System.out.println(turn == true ? "PLAYER 1: What do want to do?" : "PLAYER 2: What do want to do?");
+         
+            //robot choice
+          String choice = botChoice.get((int)Math.random()*3 + 1);
+        
+        if(turn == true){
+         choice = scan.nextLine();
+        }
         switch(choice){
             case "attack":
 
@@ -23,6 +33,9 @@ public class main {
             case "switch":
                 switchStick(stickA1, stickA2);
         }
+        turn != turn;
+        
+
     }
 }
     //add stick values together, if one hand equals 5, you lose that hand
@@ -40,12 +53,12 @@ public class main {
             s2 = 0;
         }
     }
-    public static boolean checkStick(int s1, int s2){
+    /* public static boolean checkStick(int s1, int s2){
         if(s1 > 5 || s2 > 5){
             return false;
         }
         return true;
-    }
+    } */
     public static void player1Turn(){
         System.out.println("Player 1's turn:");
     }
@@ -54,14 +67,14 @@ public class main {
         int total = left + right;
         Scanner scan = new Scanner(System.in);
         System.out.println("How much do you want to put on your left hand?");
+        left = scan.nextInt();
         if(total - left > 0){
             total -= left;
-            left = scan.nextInt();
         }
         System.out.println("How much do you want to put on your right hand?");
+        right = scan.nextInt();
         if(total - right > 0){
             total -= right;
-            right = scan.nextInt();
         }
     }
 }

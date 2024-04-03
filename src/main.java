57a9779dc0186e11f91caecc1260889dc9826024
Leo G <lgeppert1145@stu.neisd.net>
2024-04-03 10:42:14 -0500

    finished switchStick method

--- src/main.java
+++ src/main.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.awt.*;
 public class main {
+        static int stickA1 = 1;
+        static int stickA2 = 1;
+        static int stickB1 = 1;
+        static int stickB2 = 1;
+        static boolean turn = true; //if true, it is player 1's turn, if false, it is player 2's turn
     public static void main(String[] args) throws Exception {
         boolean game = true;
-        int stickA1 = 1;
-        int stickA2 = 1;
-        int stickB1 = 1;
-        int stickB2 = 1;
-        boolean turn = true; //if true, it is player 1's turn, if false, it is player 2's turn
         Map<Integer, String> botChoice = new TreeMap<>();
         botChoice.put(1, "attack");
         botChoice.put(2, "switch");
         
+        
+
         System.out.println("Sticks");
         Scanner scan = new Scanner(System.in);
         //game will run while the value is true
@@ -44,39 +46,49 @@ public class main {
     //add stick values together, if one hand equals 5, you lose that hand
     public static void addStick(int a1, int a2, int b1, int b2){
             Scanner scan = new Scanner(System.in);
-
+            String hand = "";
+            String attack = "";
+            Map<Integer, String> leftOrRight = new TreeMap<>();
+                leftOrRight.put(1, "left");
+                leftOrRight.put(2, "right");
+            
             System.out.println("With what hand? (left/right)");//if left, then put a1 into addStick method
-            String hand = scan.nextLine();
+            if(turn == true){
+             hand = scan.nextLine();}
+            else{
+                hand = leftOrRight.get((int)Math.random()*3 + 1);
+            }
             System.out.println("To whom? (left/right)");
-            String attack = scan.nextLine();
+            if(turn == true){ attack = scan.nextLine();
+            }
+            else{
+                attack = leftOrRight.get((int)Math.random()*3 + 1);
+            }
             int s1 = hand.equals("left") ? a1 : a2;
             int s2 = attack.equals("left") ? b1 : b2;
-
         s2 +=s1;
         if(s2 == 5){
             s2 = 0;
-        }
-    }
-    /* public static boolean checkStick(int s1, int s2){
-        if(s1 > 5 || s2 > 5){
-            return false;
-        }
-        return true;
-    } */
-    public static void player1Turn(){
-        System.out.println("Player 1's turn:");
+        }                                                                                                                                                                                                                                      
     }
     //Switch stick values for the hand, make sure to later add that you cant just infinitely switch around the same two values
     public static void switchStick(int left, int right){
         int total = left + right;
+        int leftAmount;
+        int rightAmount;
         Scanner scan = new Scanner(System.in);
+        Map<Integer, String> leftOrRight = new TreeMap<>();
+        leftOrRight.put(1, "left");
+        leftOrRight.put(2, "right");
         System.out.println("How much do you want to put on your left hand?");
-        left = scan.nextInt();
+        if(turn == true){leftAmount = scan.nextInt();}
+        else{leftAmount = ((int)Math.random()*6);}
         if(total - left > 0){
             total -= left;
         }
         System.out.println("How much do you want to put on your right hand?");
-        right = scan.nextInt();
+        if(turn == true){rightAmount = scan.nextInt();}
+        else{rightAmount = ((int)Math.random()*6);}
         if(total - right > 0){
             total -= right;
         }
